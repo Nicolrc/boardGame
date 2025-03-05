ecccd71ef9647686d9d2a90440595f680b86b983
Nicolrc <nicolas.leriche@viacesi.fr>
2025-03-05 13:56:50 +0100

    connect bdd

--- technoBoost/boardGames/src/main/java/com/boost/boardgames/modele/Boardgame.java
+++ technoBoost/boardGames/src/main/java/com/boost/boardgames/modele/Boardgame.java
@@ -1,4 +1,40 @@
 package com.boost.boardgames.modele;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "boardgame")
 public class Boardgame {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_Boardgame;
+
+    @Getter
+    @Setter
+    @Column(name = "game_nom")
+    private String GameNom;
+
+    @Column(name = "joueur_min")
+    private Integer JoueurMin;
+
+    @Column(name = "joueur_max")
+    private Integer JoueurMax;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categorie")
+    private Categorie Categorie;
+
+    public Boardgame() {
+
+    }
+
+    public Boardgame(String GameNom, Integer JoueurMin, Integer JoueurMax, Categorie Categorie) {
+        this.GameNom = GameNom;
+        this.JoueurMin = JoueurMin;
+        this.JoueurMax = JoueurMax;
+        this.Categorie = Categorie;
+    }
+
 }
--- technoBoost/boardGames/src/main/java/com/boost/boardgames/modele/Categorie.java
+++ technoBoost/boardGames/src/main/java/com/boost/boardgames/modele/Categorie.java
@@ -1,4 +1,28 @@
 package com.boost.boardgames.modele;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Categorie")
 public class Categorie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_Categorie;
+
+    @Getter
+    @Setter
+    @Column(name = "cat_nom")
+    private String catnom;
+
+    public Categorie() {
+
+    }
+
+    public Categorie(String catnom) {
+        this.catnom = catnom;
+    }
+
 }
